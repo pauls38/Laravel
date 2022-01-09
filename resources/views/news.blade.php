d2e38271e565eb96e41a25f80079d4c499ad760f
pauls38 <pauls38@tut.by>
2022-01-09 20:11:19 +0300

    na VS code.

---
+++ resources/views/news.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Первая новость в проекте - Чиф - это чрезвычайно интересная фигура.</h1>   
+</body>
+</html>
